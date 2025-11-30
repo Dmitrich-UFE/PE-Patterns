125d8a74767aa37107237e8fbea8b87795a74668
Евтухов Дмитрий <justarcane@vk.com>
2025-11-30 19:43:47 +0300

    Создана реализация фабричного метода

---
+++ Patterns/Patterns/Melon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_method
+{
+    internal class Melon: IPlant
+    {
+        public Melon() { }
+
+        void IPlant.Grow() 
+        {
+            Console.WriteLine("Дыня выросла на 1 единицу");
+        }
+    }
+}
--- Patterns/Patterns/Program.cs
+++ Patterns/Patterns/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Patterns
+﻿using Factory_method;
+
+namespace Patterns
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Выберите тип растения: 0 - дыня, 1 - картофель");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Creator creator = Initializator.init((Initializator.PlantType)i);
+                IPlant plant = creator.CreatePlant();
+                plant.Grow();
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Неверный параметр инициализации");
+            }
+        }
+
+    }
+
+
+    internal class Initializator 
+    {
+        public enum PlantType { Melon, Potato }
+
+        internal static Creator init(PlantType type)
+        {
+            switch (type)
+            {
+                case PlantType.Melon:
+                    return new MelonCreator();
+
+                case PlantType.Potato:
+                    return new PotatoCreator();
+
+                default:
+                    return null;
+            }
         }
     }
 }
